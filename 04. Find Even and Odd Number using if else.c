ff0273453abaf3d8e6dd745252a79fcb4bda7c0f
jakafind <jakafind.id@gmail.com>
2024-07-11 08:47:44 +0600

    Update 04. Find Even and Odd Number using if else.c

--- 04. Find Even and Odd Number using if else.c
+++ 04. Find Even and Odd Number using if else.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-int main ()
-{
-float a, b, c;
 
-printf("Enter first number: ");
-scanf("%f", &a);
-printf("Enter first number: ");
-scanf("%f", &b);
-
-c=a+b;
-
-printf("Sum = %.2f", c);
-  
-return 0;
+int main() {
+   int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    
+    if(a%2==0){
+        printf("This is an Even Number");
+    }
+else
+printf("This is a Odd Number");
+    return 0;
 }
