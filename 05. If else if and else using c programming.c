f05c558876b31bc78766e3cd182bafaaf587dc47
jakafind <jakafind.id@gmail.com>
2024-07-11 08:46:35 +0600

    Create 05. If else if and else using c programming.c

---
+++ 05. If else if and else using c programming.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    
+    if(a==1252){
+        printf("Jakaria");
+    }
+else if(a==1292){
+    printf("Trisha");
+}
+else
+printf("Not Found");
+    return 0;
+}
